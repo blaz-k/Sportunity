19fa23ba7b176cec2015e63e591877c542181e11
blaz <blazyy@gmail.com>
2021-07-20 12:08:48 +0200

    added templates and handlers: about, contact, registration, shop

--- handlers/public.py
+++ handlers/public.py
@@ -7,3 +7,19 @@ from models.settings import db
 
 def home():
     return render_template("public/index.html")
+
+
+def shop():
+    return render_template("public/shop.html")
+
+
+def about():
+    return render_template("public/about.html")
+
+
+def contact():
+    return render_template("public/contact.html")
+
+
+def registration():
+    return render_template("public/registration.html")
--- main.py
+++ main.py
@@ -10,6 +10,15 @@ db.create_all()
 
 # PUBLIC
 app.add_url_rule(rule="/", endpoint="public.home", view_func=public.home, methods=["GET"])
+app.add_url_rule(rule="/shop", endpoint="public.shop", view_func=public.shop, methods=["GET"])
+app.add_url_rule(rule="/about", endpoint="public.about", view_func=public.about, methods=["GET"])
+app.add_url_rule(rule="/contact", endpoint="public.contact", view_func=public.contact, methods=["GET"])
+
+# AUTHENTICATION
+app.add_url_rule(rule="/registration", endpoint="public.registration", view_func=public.registration, methods=["GET"])
+
+
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
