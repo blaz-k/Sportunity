7160172f28b773776a944025a7d86141b2a052ee
Blaz <blazyy@gmail.com>
2021-12-01 11:14:49 +0100

    smartninja_redis changed with redis

--- models/settings.py
+++ models/settings.py
@@ -1,8 +1,8 @@
 import os
-import smartninja_redis
+import redis as lib_redis
 from sqla_wrapper import SQLAlchemy
 
-redis = smartninja_redis.from_url(os.environ.get("REDIS_URL"))
+redis = lib_redis.from_url(os.environ.get("REDIS_URL"))
 
 db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(db_url)
